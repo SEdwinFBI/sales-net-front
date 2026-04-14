501b34a75d7eca93a616b7f0fa8547c9d02e78c7
SEdwinFBI <baquiaxfernando0@gmail.com>
2026-04-13 22:15:41 -0600

    fix: button cart on tablet and desktop

--- src/features/sales/components/CartButton.tsx
+++ src/features/sales/components/CartButton.tsx
@@ -7,6 +7,7 @@ import { formatCurrency } from '@/helpers/money'
 
 
 
+
 const CartButton = () => {
   const items = useSalesStore((state) => state.items)
   const openCart = useSalesStore((state) => state.openCart)
@@ -15,24 +16,27 @@ const CartButton = () => {
   const total = items.reduce((sum, item) => sum + item.qty * item.price, 0)
 
   return (
-    <Button
-      className="fixed inset-x-0 bottom-0 mx-2 mb-23 flex items-center justify-between rounded-[1rem] px-4"
-      size="md"
-      onClick={openCart}
-    >
-      <div className="flex items-center gap-3">
-        <span className="relative">
-          <ShoppingCart className="font-bold" />
-          <span className="absolute -top-2 -right-2 rounded-full bg-green-700 px-1.5 text-xs text-white">
-            {totalItems}
+    <>
+
+      <Button
+        className="fixed right-0  max-[420px]:inset-x-0  bottom-0 mx-2 mb-10 flex items-center justify-between rounded-[1rem] px-4"
+        size="md"
+        onClick={openCart}
+      >
+        <div className="flex items-center gap-3">
+          <span className="relative">
+            <ShoppingCart className="font-bold" />
+            <span className="absolute -top-2 -right-2 rounded-full bg-green-700 px-1.5 text-xs text-white">
+              {totalItems}
+            </span>
           </span>
-        </span>
 
-        <span>Carrito ({totalItems} items)</span>
-      </div>
+          <span>Carrito ({totalItems} items)</span>
+        </div>
 
-      <span className="font-semibold">{formatCurrency(total)}</span>
-    </Button>
+        <span className="font-semibold">{formatCurrency(total)}</span>
+      </Button>
+    </>
   )
 }
 
